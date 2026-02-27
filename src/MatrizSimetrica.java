c3aced7ec9c2518982e006419ad52ea074ba2a8f
Dibo72 <humanida133@gmail.com>
2026-02-27 09:28:25 +0100

    Ejercicio acabado

--- src/MatrizSimetrica.java
+++ src/MatrizSimetrica.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MatrizSimetrica {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[][] matriz = new int[3][3];
+        boolean simetrico = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                matriz[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                if (matriz[i][j] != matriz[j][i]){
+                    simetrico = false;
+                }
+            }
+        }
+        System.out.println("Matriz:");
+        System.out.println();
+        for (int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        if(simetrico){
+            System.out.println("Matriz simetrica");
+        }else {
+            System.out.println("Matriz no simetrica");
         }
     }
 }
